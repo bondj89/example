e3b1acc092e88400cab89702ae81d276b49e7937
Bond <jobocuban@gmail.com>
2019-09-30 22:23:41 -0600

    Initial commit

---
+++ src/Example.java
@@ -0,0 +1,13 @@
+/*
+This is a simple Java Program.
+
+Call this file Example.Java.
+ */
+
+public class Example {
+  // A Java program begins with a call to main().
+  public static void main(String[] args) {
+    System.out.println("Java drives the Web.");
+  }
+
+}
